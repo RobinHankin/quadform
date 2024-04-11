f8d6145c8d8ab226af5e1cb78c9753ebf77b5ea1
robin <hankin.robin@gmail.com>
2024-04-11 18:48:23 +0100

    correct blurb

--- tests/testthat/test_aaa.R
+++ tests/testthat/test_aaa.R
@@ -1,9 +1,6 @@
 ## This file follows the structure of aaa.R in the free group package.
 
-## Define some checker functions, and call them at the end.  They
-## should all return TRUE if the package works, and stop with error if
-## a test is failed.  Function checker1() has one argument, checker2()
-## two, and checker3() has three.  
+## Define a checker function, and call it at the end.  
 
 test_that("Test suite aaa.R",{
 
