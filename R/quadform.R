01fd71ec23a40c11b412752af4615ed52c10a45a
robin <hankin.robin@gmail.com>
2024-04-21 17:39:40 +0100

    quad.3form() and quad.3tform()

--- R/quadform.R
+++ R/quadform.R
@@ -25,12 +25,27 @@
 
 `quad.form.inv` <- function (M, x){ drop(cprod(x, solve(M, x))) }
 
-`quad.3form` <- function(M,left,right){ crossprod(crossprod(M, Conj(left)), right) }
+`quad.3form_ab` <- function(M,left,right){ crossprod(crossprod(M, Conj(left)), right) }
+`quad.3form_bc` <- function(M,left,right){ cprod(left, (M %*% right)) }
+`quad.3form` <- function(M,left,right){
+    if(ncol(left) < ncol(right)){
+        quad.3form_ab(M,left,right)
+    } else {
+        quad.3form_bc(M,left,right)
+    }
+}
 
 `quad.3form.inv` <- function(M,left,right){ drop(cprod(left, solve(M, right))) }
 
-`quad.3tform` <- function(M,left,right){ tcrossprod(left, tcrossprod(Conj(right), M)) }
-
+`quad.3tform_ab` <- function(M,left,right){ tcprod(left %*% M,right)}
+`quad.3tform_bc` <- function(M,left,right){ tcrossprod(left, tcrossprod(Conj(right), M)) }
+`quad.3tform` <- function(M,left,right){
+    if(nrow(left) < nrow(right)){
+        quad.3tform_ab(M,left,right)
+    } else {
+        quad.3tform_bc(M,left,right)
+    }
+}
 `quad.tform` <- function(M,x){ tcrossprod(x, tcrossprod(Conj(x), M)) }
 
 `quad.tform.inv` <- function(M,x){ drop(quad.form.inv(M, ht(x))) }
